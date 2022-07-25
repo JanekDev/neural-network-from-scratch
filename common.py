0c683705a0e9e374973c7ba028e3cffdecaa7cd7
JanekDev <m.wilinski11@gmail.com>
2022-07-25 16:23:19 +0200

    mnist overhaul

--- common.py
+++ common.py
@@ -1,75 +1,37 @@
-#implement common functions
-from unittest import result
 import numpy as np
-import matplotlib.pyplot as plt
-def plot_iris_data(iris):
-    #copy iris
-    iris_ = iris.copy()
-    iris_['color'] = iris_['Species'].map({'Iris-setosa': 'r', 'Iris-versicolor': 'g', 'Iris-virginica': 'b'})
-    #plot iris data on different plots
-    plt.figure(figsize=(20,10))
-    plt.subplot(2,3,1)
-    plt.scatter(iris_['SepalLength'], iris_['SepalWidth'], c=iris_['color'])
-    plt.title('Sepal Length vs Sepal Width')
-    plt.subplot(2,3,2)
-    plt.scatter(iris_['PetalLength'], iris_['PetalWidth'], c=iris_['color'])
-    plt.title('Petal Length vs Petal Width')
-    plt.subplot(2,3,3)
-    plt.scatter(iris_['SepalLength'], iris_['PetalLength'], c=iris_['color'])
-    plt.title('Sepal Length vs Petal Length')
-    plt.subplot(2,3,4)
-    plt.scatter(iris_['SepalWidth'], iris_['PetalWidth'], c=iris_['color'])
-    plt.title('Sepal Width vs Petal Width')
-    plt.subplot(2,3,5)
-    plt.scatter(iris_['SepalLength'], iris_['PetalWidth'], c=iris_['color'])
-    plt.title('Sepal Length vs Petal Width')
-    plt.subplot(2,3,6)
-    plt.scatter(iris_['SepalWidth'], iris_['PetalLength'], c=iris_['color'])
-    plt.title('Sepal Width vs Petal Length')
-    plt.show()
 
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 def sigmoid_prime(x):
-    return sigmoid(x)*(1-sigmoid(x))
+    return sigmoid(x) * (1 - sigmoid(x))
 
-def mse(y_true, y_pred):
-    return np.mean(np.power(y_true-y_pred, 2));
+def load_mnist(path, kind='train'):
+    import os
+    import gzip
+    import numpy as np
 
-def cross_entropy(y_true, y_pred):
-    N = y_pred.shape[0]
-    return -np.sum(y_true * np.log(y_pred)) / N
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte.gz'
+                               % kind)
+    images_path = os.path.join(path,
+                               '%s-images-idx3-ubyte.gz'
+                               % kind)
 
-def cross_entropy_prime(y_true, y_pred):
-    return y_pred - y_true;
+    with gzip.open(labels_path, 'rb') as lbpath:
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8, offset=8)
 
-def mse_prime(y_true, y_pred):
-    return 2*(y_pred-y_true)/y_true.size;
+    with gzip.open(images_path, 'rb') as imgpath:
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8, offset=16).reshape(len(labels), 784)
 
-def tanh(x):
-    return np.tanh(x);
+    # reshape so that each image is a flattened vector of 28x28 pixels
+    images = images.reshape(images.shape[0], 1, 784)
+    
+    return images, labels
 
-def tanh_prime(x):
-    return 1-np.tanh(x)**2;
+def cross_entropy(y, y_hat):
+    return -np.sum(y * np.log(y_hat))
 
-def softmax(x):
-    return np.exp(x)/np.sum(np.exp(x));
-
-def softmax_prime(x):
-    return softmax(x)*(1-softmax(x));
-
-def to_categorical(y, num_classes=None, dtype='float32'):
-    y = np.array(y, dtype='int')
-    input_shape = y.shape
-    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-        input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
-    if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=dtype)
-    categorical[np.arange(n), y] = 1
-    output_shape = input_shape + (num_classes,)
-    categorical = np.reshape(categorical, output_shape)
-    return categorical
+def cross_entropy_prime(y, y_hat):
+    return y_hat - y
